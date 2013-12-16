8ef0fabb02bdfd5451c1e331865e900210cceb41
Matteo Munaro <matteo.munaro@dei.unipd.it>
2013-12-16 13:32:34 +0100

    Added .h and .cpp files for class DetectionSource, which contains information about nodes producing detections.

---
+++ tracking/include/open_ptrack/tracking/detection_source.h
@@ -0,0 +1,87 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2011-, Matteo Munaro [matteo.munaro@dei.unipd.it], Filippo Basso
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ * * Neither the name of the copyright holder(s) nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef DETECTION_SOURCE_H_
+#define DETECTION_SOURCE_H_
+
+#include <opencv2/opencv.hpp>
+#include <Eigen/Eigen>
+
+#include <open_ptrack/detection/conversions.h>
+#include <tf/tf.h>
+
+namespace open_ptrack
+{
+  namespace tracking
+  {
+
+    class DetectionSource
+    {
+      protected:
+        cv::Mat image_;
+        tf::StampedTransform transform_;
+        tf::StampedTransform inverse_transform_;
+        Eigen::Matrix3d intrinsic_matrix_;
+        ros::Time time_;
+        ros::Duration duration_;
+        std::string frame_id_;
+
+      public:
+        DetectionSource(cv::Mat image, tf::StampedTransform transform, tf::StampedTransform inverse_transform,
+            Eigen::Matrix3d intrinsic_matrix, ros::Time time, std::string frame_id);
+
+        virtual ~DetectionSource();
+
+        void update(cv::Mat image, tf::StampedTransform transform, tf::StampedTransform inverse_transform,
+            Eigen::Matrix3d intrinsic_matrix, ros::Time time, std::string frame_id);
+
+        Eigen::Vector3d transform(const Eigen::Vector3d& v);
+        Eigen::Vector3d transform(const geometry_msgs::Vector3& v);
+        Eigen::Vector3d inverseTransform(const Eigen::Vector3d& v);
+        Eigen::Vector3d inverseTransform(const geometry_msgs::Vector3& v);
+        Eigen::Vector3d world2cam(const Eigen::Vector3d& v);
+        Eigen::Vector3d transformToCam(const Eigen::Vector3d& v);
+        cv::Mat& getImage();
+        ros::Time getTime();
+        ros::Duration getDuration();
+        std::string getFrameId();
+        void setImage(cv::Mat& image);
+    };
+
+  } /* namespace tracking */
+} /* namespace open_ptrack */
+
+#endif /* DETECTION_SOURCE_H_ */
---
+++ tracking/src/detection_source.cpp
@@ -0,0 +1,151 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2011-, Matteo Munaro [matteo.munaro@dei.unipd.it], Filippo Basso
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ * * Neither the name of the copyright holder(s) nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <open_ptrack/tracking/detection_source.h>
+
+namespace open_ptrack
+{
+  namespace tracking
+  {
+
+    DetectionSource::DetectionSource(cv::Mat image, tf::StampedTransform transform,
+        tf::StampedTransform inverse_transform, Eigen::Matrix3d intrinsic_matrix, ros::Time time, std::string frame_id) :
+	    image_(image), transform_(transform), inverse_transform_(inverse_transform),
+	    intrinsic_matrix_(intrinsic_matrix), time_(time), duration_(0), frame_id_(frame_id)
+    {
+
+    }
+
+    DetectionSource::~DetectionSource()
+    {
+
+    }
+
+    void DetectionSource::update(cv::Mat image, tf::StampedTransform transform,
+        tf::StampedTransform inverse_transform, Eigen::Matrix3d intrinsic_matrix, ros::Time time, std::string frame_id)
+    {
+      image_ = image;
+      transform_ = transform;
+      inverse_transform_ = inverse_transform;
+      intrinsic_matrix_ = intrinsic_matrix;
+      duration_ = time - time_;
+      time_ = time;
+      frame_id_ = frame_id;
+    }
+
+    Eigen::Vector3d DetectionSource::transform(const Eigen::Vector3d& v)
+    {
+      Eigen::Vector3d ret;
+      tf::Vector3 t(v(0), v(1), v(2));
+      t = transform_(t);
+      ret(0) = t.getX();
+      ret(1) = t.getY();
+      ret(2) = t.getZ();
+      return ret;
+    }
+
+    Eigen::Vector3d DetectionSource::transform(const geometry_msgs::Vector3& v)
+    {
+      Eigen::Vector3d ret;
+      tf::Vector3 t;
+      tf::vector3MsgToTF(v, t);
+      t = transform_(t);
+      ret(0) = t.getX();
+      ret(1) = t.getY();
+      ret(2) = t.getZ();
+      return ret;
+    }
+
+    Eigen::Vector3d DetectionSource::inverseTransform(const Eigen::Vector3d& v)
+    {
+      Eigen::Vector3d ret;
+      tf::Vector3 t(v(0), v(1), v(2));
+      //ROS_INFO("C: %f %f %f", t.getX(), t.getY(), t.getZ());
+      t = inverse_transform_(t);
+      ret(0) = t.getX();
+      ret(1) = t.getY();
+      ret(2) = t.getZ();
+      return ret;
+    }
+
+    Eigen::Vector3d DetectionSource::inverseTransform(const geometry_msgs::Vector3& v)
+    {
+      Eigen::Vector3d ret;
+      tf::Vector3 t;
+      tf::vector3MsgToTF(v, t);
+      t = inverse_transform_(t);
+      ret(0) = t.getX();
+      ret(1) = t.getY();
+      ret(2) = t.getZ();
+      return ret;
+    }
+
+    Eigen::Vector3d DetectionSource::world2cam(const Eigen::Vector3d& v)
+    {
+      return open_ptrack::detection::Conversions::world2cam(v, intrinsic_matrix_);
+    }
+
+    Eigen::Vector3d DetectionSource::transformToCam(const Eigen::Vector3d& v)
+    {
+      return open_ptrack::detection::Conversions::world2cam(inverseTransform(v), intrinsic_matrix_);
+    }
+
+    cv::Mat& DetectionSource::getImage()
+    {
+      return image_;
+    }
+
+    ros::Time DetectionSource::getTime()
+    {
+      return time_;
+    }
+
+    ros::Duration DetectionSource::getDuration()
+    {
+      return duration_;
+    }
+
+    std::string DetectionSource::getFrameId()
+    {
+      return frame_id_;
+    }
+
+    void DetectionSource::setImage(cv::Mat& image)
+    {
+      image_ = image;
+    }
+  } /* namespace tracking */
+} /* namespace open_ptrack */
