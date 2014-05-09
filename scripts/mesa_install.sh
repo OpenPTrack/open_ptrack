4887a0569f015c5d6989ffcefc0e0f86b57e9908
Alex Nano <nanodust@gmail.com>
2014-05-08 18:06:59 -0700

    Update mesa_install.sh

--- scripts/mesa_install.sh
+++ scripts/mesa_install.sh
@@ -2,3 +2,9 @@
 cd ~/Downloads
 wget -U "Mozilla" http://www.mesa-imaging.ch/customer/driver/libmesasr-dev-1.0.14-748.amd64.deb
 sudo dpkg -i libmesasr-dev-1.0.14-748.amd64.deb 
+
+# or for 32 bit - if get dpkg: error processing libmesasr-dev-1.0.14-748.amd64.deb (--install):
+# package architecture (amd64) does not match system (i386)
+
+#http://downloads.mesa-imaging.ch/dlm.php?fname=./customer/driver/libmesasr-dev-1.0.14-747.i386.deb
+#sudo dpkg -i libmesasr-dev-1.0.14-747.i386.deb
