d247ec2b4f9682827d82246a3c1e063fcd04f1bd
Matteo Munaro <matteo.munaro@dei.unipd.it>
2014-08-27 10:33:23 +0200

    Minor update to ros_configure script to handle some cases when ROS forgets to use UTF-8 encoding.

--- scripts/ros_configure.sh
+++ scripts/ros_configure.sh
@@ -14,4 +14,5 @@ mkdir -p ~/workspace/ros/rosbuild
 rosws init ~/workspace/ros/rosbuild ~/workspace/ros/catkin/devel
 echo "source ~/workspace/ros/rosbuild/setup.bash" >> ~/.bashrc
 echo "export KINECT_DRIVER=openni" >> ~/.bashrc
+echo "export LC_ALL=C" >> ~/.bashrc
 
