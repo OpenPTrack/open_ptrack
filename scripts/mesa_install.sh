4a9e35660dff8cee3f900fdd0f38cfb9542e957f
Alex Nano <nanodust@gmail.com>
2015-07-12 18:39:28 -0500

    Update mesa_install.sh

--- scripts/mesa_install.sh
+++ scripts/mesa_install.sh
@@ -1,6 +1,6 @@
 # Install Mesa SwissRanger driver:
 cd ~/Downloads
-wget -U "Mozilla" http://www.mesa-imaging.ch/customer/driver/libmesasr-dev-1.0.14-748.amd64.deb
+wget -U "Mozilla" http://downloads.mesa-imaging.ch/dlm.php?fname=./customer/driver/libmesasr-dev-1.0.14-748.amd64.deb
 sudo dpkg -i libmesasr-dev-1.0.14-748.amd64.deb 
 
 # or for 32 bit - if get dpkg: error processing libmesasr-dev-1.0.14-748.amd64.deb (--install):
